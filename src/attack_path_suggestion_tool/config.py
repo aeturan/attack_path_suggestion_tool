880e15c15584b1122a87502d1643141d53ab87c2
Ahmet Erkan Turan <erkan.turan@tum.de>
2025-11-12 17:50:31 +0100

    bugfix: sessions path

--- src/attack_path_suggestion_tool/config.py
+++ src/attack_path_suggestion_tool/config.py
@@ -3,9 +3,11 @@ from pathlib import Path
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+PACKAGE_DIR = Path(__file__).resolve().parent
+
 
 class StorageConfig(BaseSettings):
-    sessions_dir: Path = Field(default=Path("sessions"), description="Directory to store graph session files.")
+    sessions_dir: Path = Field(default=PACKAGE_DIR / "sessions", description="Directory to store graph session files.")
 
 class AnalysisConfig(BaseSettings):
     num_paths_to_find: int = Field(default=5, gt=0, description="The default number of attack paths to generate.")
